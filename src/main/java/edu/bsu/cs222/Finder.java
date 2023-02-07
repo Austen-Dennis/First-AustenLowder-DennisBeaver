ba5a8c217b695688418081764e6ce9a02f676630
AustenLowder2002 <112116586+AustenLowder2002@users.noreply.github.com>
2023-02-07 12:48:54 -0500

    Started the initial method in finder to locate a wikipedia article.

--- src/main/java/edu/bsu/cs222/Finder.java
+++ src/main/java/edu/bsu/cs222/Finder.java
@@ -1,4 +1,16 @@
 package edu.bsu.cs222;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Finder {
+
+    public Finder() throws IOException {
+        URL url = new URL("https://en.wikipedia.org");
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent","Revision Reporter/0.1 (me@bsu.edu)");
+        InputStream inputStream = connection.getInputStream();
+    }
 }
