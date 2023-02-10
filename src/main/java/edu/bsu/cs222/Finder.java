7b158718ec7d4f365e82b3b2aa98e46555a2acf1
AustenLowder2002 <112116586+AustenLowder2002@users.noreply.github.com>
2023-02-10 13:53:59 -0500

    FIxed spelling errors that I created

--- src/main/java/edu/bsu/cs222/Finder.java
+++ src/main/java/edu/bsu/cs222/Finder.java
@@ -19,10 +19,10 @@ public class Finder {
     public static URL URLBuilder(String articleSearch) throws IOException {
         String articleName = articleSearch.replace(" ", "%20");
         URL url = new URL("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" + articleName +"=timestamp|user&rvlimit=27&redirects");
-        conncector(url);
+        connector(url);
         return url;
     }
-    public static void conncector(URL url) throws IOException {
+    public static void connector(URL url) throws IOException {
         URLConnection connection = url.openConnection();
         connection.setRequestProperty("User-Agent", "Revision Reporter/0.1 (austen.lowder@bsu.edu)");
         InputStream inputStream = connection.getInputStream();
--- src/main/java/edu/bsu/cs222/Printer.java
+++ src/main/java/edu/bsu/cs222/Printer.java
@@ -3,11 +3,10 @@ package edu.bsu.cs222;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.net.URLConnection;
 
 public class Printer {
-    public Printer(URLConnection connection) throws IOException {
+    public void JSONPrinter(URLConnection connection) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String inputLine;
         while ((inputLine = in.readLine()) != null)
