9c118bc101f1036928afdd77a8b8d7ac5711739f
DennisRBeaver <drbeaver2@bsu.edu>
2023-02-18 15:29:37 -0500

    Adds classes for GUI.

---
+++ src/main/java/edu/bsu/cs222/SearchView.java
@@ -0,0 +1,23 @@
+package edu.bsu.cs222;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SearchView extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("resources/search-view.fxml"));
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+        public static void main(String[] args) { launch(); }
+}
