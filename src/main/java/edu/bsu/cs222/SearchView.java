16e5c09405716eb56d5fcc90148c10bc04482dee
AustenLowder2002 <112116586+AustenLowder2002@users.noreply.github.com>
2023-02-19 17:01:49 -0500

    Adds module-info java file

--- src/main/java/edu/bsu/cs222/SearchView.java
+++ src/main/java/edu/bsu/cs222/SearchView.java
@@ -2,6 +2,7 @@ package edu.bsu.cs222;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,10 +12,10 @@ import java.io.IOException;
 public class SearchView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(SearchView.class.getResource("search-view.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Wiki Search");
-        //stage.setScene(scene);
+
+
+        stage.setTitle("FXML Welcome");
+
         stage.show();
     }
 
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module FirstProject.main {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens edu.bsu.cs222.SearchView to javafx.fxml;
+    exports edu.bsu.cs222;
+}
