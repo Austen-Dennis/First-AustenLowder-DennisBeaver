597ba6f05edab171a357e65eca98646da3229810
DennisRBeaver <drbeaver2@bsu.edu>
2023-02-19 16:16:19 -0500

    Adds gui back to main again.

---
+++ src/main/java/edu/bsu/cs222/TestApp.java
@@ -0,0 +1,21 @@
+package edu.bsu.cs222;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TestApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+    }
+}
