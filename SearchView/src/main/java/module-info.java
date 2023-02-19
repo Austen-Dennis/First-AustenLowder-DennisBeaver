0623b8516c89030e71be786f57d486f89039d4b0
DennisRBeaver <drbeaver2@bsu.edu>
2023-02-19 14:10:32 -0500

    Resets the gui and separates it from main.

---
+++ SearchView/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module edu.bsu.cs222.searchview {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens edu.bsu.cs222.searchview to javafx.fxml;
+    exports edu.bsu.cs222.searchview;
+}
