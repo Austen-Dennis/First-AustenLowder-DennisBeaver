46eb72a3170218d7313dc89b87e342cb67170e0f
AustenLowder2002 <112116586+AustenLowder2002@users.noreply.github.com>
2023-02-19 18:40:26 -0500

    Add files via upload

---
+++ src/main/java/edu/bsu/cs222/Controller.java
@@ -0,0 +1,18 @@
+package edu.bsu.cs222;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    public TextField searchValue;
+
+
+    @FXML
+    public void onSearch(ActionEvent actionEvent) {
+        String articleSearch = searchValue.getText();
+        System.out.println(articleSearch);
+    }
+}
