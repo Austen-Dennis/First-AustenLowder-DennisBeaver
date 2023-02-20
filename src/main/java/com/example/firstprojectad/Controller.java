9401c5b50a9eb59cf23e7bd6aae1c6d3f1196d6d
DennisRBeaver <drbeaver2@bsu.edu>
2023-02-19 19:22:35 -0500

    Redesigns gui.

---
+++ src/main/java/com/example/firstprojectad/Controller.java
@@ -0,0 +1,21 @@
+package com.example.firstprojectad;
+
+import edu.bsu.cs222.ArticleInfo;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import edu.bsu.cs222.Finder;
+import
+
+import java.io.IOException;
+
+public class Controller {
+    @FXML
+    public TextField searchValue;
+
+    @FXML
+    public void onSearch(ActionEvent actionEvent) throws IOException {
+        String articleSearch = searchValue.getText();
+    }
+}
