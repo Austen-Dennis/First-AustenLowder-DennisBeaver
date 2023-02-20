7cf05f7458056eb28839961424235b67159879bb
DennisRBeaver <drbeaver2@bsu.edu>
2023-02-19 20:05:54 -0500

    Adds printing functions and acceptance for user input.

--- src/main/java/com/example/firstprojectad/Controller.java
+++ src/main/java/com/example/firstprojectad/Controller.java
@@ -1,13 +1,14 @@
 package com.example.firstprojectad;
 
-import edu.bsu.cs222.ArticleInfo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import edu.bsu.cs222.Finder;
 
 import java.io.IOException;
+
+import static edu.bsu.cs222.ArticleInfo.*;
+import static edu.bsu.cs222.ArticleInfo.revisionList;
+import edu.bsu.cs222.Finder;
 
 public class Controller {
     @FXML
@@ -16,5 +17,18 @@ public class Controller {
     @FXML
     public void onSearch(ActionEvent actionEvent) throws IOException {
         String articleSearch = searchValue.getText();
-    }
+        new Finder(articleSearch);
+            for (int user : userList.keySet()) {
+                System.out.print(user + 1 + " ");
+                // formats timestamplist as it iterates and prints out a proper date
+                System.out.print("Date: " + timestampList.get(user).toString().replace("T", "     Time: ").replace("Z", "") + "     ");
+                System.out.print("Name: " + userList.get(user) + " ");
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println("Redirected to: " + redirect(articleSearch));
+            System.out.println("All revisions: " + revisionList);
+            System.exit(0);
+
+        }
 }
--- src/main/java/com/example/firstprojectad/SearchView.java
+++ src/main/java/com/example/firstprojectad/SearchView.java
@@ -12,9 +12,10 @@ import java.io.IOException;
 import static edu.bsu.cs222.ArticleInfo.*;
 
 public class SearchView extends Application {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         launch();
     }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SearchView.class.getResource("search-view.fxml"));
@@ -22,25 +23,6 @@ public class SearchView extends Application {
         stage.setTitle("FXML Loader");
         stage.setScene(scene);
         stage.show();
-        Button btn = new Button();
-        btn.setText("Search Article");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                for (int user : userList.keySet()) {
-                    System.out.print(user + 1 + " ");
-                    // formats timestamplist as it iterates and prints out a proper date
-                    System.out.print("Date: " + timestampList.get(user).toString().replace("T", "     Time: ").replace("Z", "") + "     ");
-                    System.out.print("Name: " + userList.get(user) + " ");
-                    System.out.println();
-                }
-                System.out.println();
-                System.out.println("Redirected to: " + redirect(searchValue));
-                System.out.println("All revisions: " + revisionList);
-                System.exit(0);
-
-            }
-        });
     }
 }
 
